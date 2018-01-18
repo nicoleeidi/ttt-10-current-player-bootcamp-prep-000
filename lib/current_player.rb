74bd76b84f82da893f10ae508013aebc78b263d6
Nicole Eidi <nicoleeidi1@gmail.com>
2018-01-18 16:21:39 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,9 +2,16 @@ def count(board)
   counter = 0
   board.each do |index|
     if index== "X" || index== "O"
-      counter += 1 
+      counter += 1
     end
   end
   return counter
-end 
+end
 
+def current_player(board)
+  if turn_count(board) % 2 ==0
+    return "X"
+  elsif turn_count(board) % 2 ==1
+    return "O"
+  end
+end 
